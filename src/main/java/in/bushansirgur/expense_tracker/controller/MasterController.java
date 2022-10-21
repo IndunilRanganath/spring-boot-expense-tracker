2c87dc4950589061cf50da55f2a1e26e479c7821
IndunilRanganath <indunilranganath071@gmail.com>
2022-10-21 20:12:29 +0530

    used lombok for entity

--- src/main/java/in/bushansirgur/expense_tracker/controller/MasterController.java
+++ src/main/java/in/bushansirgur/expense_tracker/controller/MasterController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import in.bushansirgur.expense_tracker.model.Expense;
@@ -15,7 +16,7 @@ public class MasterController {
 	
 	@Autowired
 	ExpenseService expenseService;
-	
+	@ResponseBody
 	@RequestMapping("/")
 	public ModelAndView home() {
 		ModelAndView mav = new ModelAndView("home");
--- src/main/java/in/bushansirgur/expense_tracker/model/Expense.java
+++ src/main/java/in/bushansirgur/expense_tracker/model/Expense.java
@@ -9,22 +9,32 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
 @Entity
 @Table(name = "tbl_expenses")
+
 @Setter
 @Getter
 @ToString
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+
+
 public class Expense {
 	
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Long id;
 	
-	@Column(name="description")
+	@Column(name="discription")
 	private String expensename;
 	
 	private BigDecimal amount;
